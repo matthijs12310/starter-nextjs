d6df9cd6e3201aceed04283cbef750a232c8db37
Matthijs Stoffer <63472519+matthijs12310@users.noreply.github.com>
2022-11-12 17:40:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
-const next = require('next')
-const http = require('http')
+var http = require("http");
 
-// init next
-const app = next(__dirname)
-const handle = app.getRequestHandler()
+http.createServer(function (request, response) {
+    // Send the HTTP header 
+    // HTTP Status: 200 : OK
+    // Content Type: text/plain
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    const { spawn } = require('child_process')
 
-// init normal http server
-// createServer(handle).listen(process.env.PORT || 3000)
+// start the `ping google.com` command
+const command = spawn('lscpu')
 
-
-
-// Create a local server to receive data from
-const server = http.createServer((req, res) => {
-  res.removeHeader('Transfer-Encoding');
-  handle(req,res)
-});
-
-server.listen(process.env.PORT || 3000);
+// the `data` event is fired every time data is
+// output from the command
+command.stdout.on('data', output => {
+    // the output data is captured and printed in the callback
+    console.log("Output: ", output.toString())
+})
+    // Send the response body as "Hello World"
+    response.end('Hello World\n');
+ }).listen(8081);
+ 
+ // Console will print the message
+ console.log('Server running at http://127.0.0.1:8081/');
